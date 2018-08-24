bc797ac0cf10523b655526dd514b5e2fecb3e1a1
lijunqi <lijq@zjitd.com>
2018-08-24 15:20:44 +0800

    。。。

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h2>About Index page</h2>
+  <ul>
+    <li>
+      <nuxt-link :to="{name:'index'}">Home</nuxt-link>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  transition: 'test'
+}
+</script>
---
+++ pages/news/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h2>News Index page</h2>
+  <p>NewsID:{{$route.params.newsId}}</p>
+  <ul>
+    <li>
+      <nuxt-link :to="{name:'index'}">Home</nuxt-link>
+    </li>
+    <li>
+      <nuxt-link :to="{name:'news-id',params:{id:123, title:'jspang.com'}}">News-123</nuxt-link>
+    </li>
+    <li>
+      <nuxt-link :to="{name:'news-id',params:{id:456}}">News-456</nuxt-link>
+    </li>
+  </ul>
+</div>
+</template>
